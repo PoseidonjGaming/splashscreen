9df3d93213370efd6c578ecb1c487922e5780e9c
lucas <lucasmagnier@free.fr>
2020-11-19 08:46:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/ListFiche.java
@@ -0,0 +1,12 @@
+package com.example.splashscreen;
+
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ListFiche extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Fiche> lesSerie=new ArrayList<Fiche>();
+}
---
+++ app/src/main/java/com/example/splashscreen/Visiteur.java
@@ -0,0 +1,11 @@
+package com.example.splashscreen;
+
+public class Visiteur {
+    public String login;
+    public String mdp;
+    public String nom;
+    public String prenom;
+    public String cp;
+    public String ville;
+    public String adr;
+}
