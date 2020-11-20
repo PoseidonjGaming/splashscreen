f7d2c34bf0ece2e2bc00ad786ed596b3426a9c4b
lucas <lucasmagnier@free.fr>
2020-11-20 12:52:24 +0100

    Ajout d'une page détail fiche

--- app/src/main/java/com/example/splashscreen/AjoutFiche.java
+++ app/src/main/java/com/example/splashscreen/AjoutFiche.java
@@ -1,20 +1,14 @@
 package com.example.splashscreen;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuFiche extends AppCompatActivity {
+public class AjoutFiche extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mune_fiche);
-    }
-
-    public void BTN_AjoutFiche(View view) {
-        startActivity(new Intent(MenuFiche.this, AjoutFiche.class));
     }
 }
--- app/src/main/java/com/example/splashscreen/Connexion.java
+++ app/src/main/java/com/example/splashscreen/Connexion.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Connexion extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
